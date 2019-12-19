aa40c737a7257ebb481c744629a72731f9bc899b
nolybear <marknolan73@gmail.com>
2019-12-19 11:04:15 +0000

    blah

---
+++ Chapter1/src/haha.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright © 2019 Lhasa Limited
+ * File created: 19 Dec 2019 by Mark Nolan
+ * Creator : Mark Nolan
+ * Version : $
+ */
+
+/**
+ * @author Davide Capomagi
+ * @since 19 Dec 2019
+ */
+public class haha
+{
+	public static void main(String args[])
+	{
+		System.out.println("haha");
+	}
+}
+/* ---------------------------------------------------------------------*
+ * This software is the confidential and proprietary
+ * information of Lhasa Limited
+ * Granary Wharf House, 2 Canal Wharf, Leeds, LS11 5PS
+ * ---
+ * No part of this confidential information shall be disclosed
+ * and it shall be used only in accordance with the terms of a
+ * written license agreement entered into by holder of the information
+ * with LHASA Ltd.
+ * --------------------------------------------------------------------- */
